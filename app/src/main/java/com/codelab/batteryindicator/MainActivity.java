483ff201e9cd1432a6d52592fe6e4d008a91d12b
ying.e.lu <ying.e.lu@accenture.com>
2021-01-10 19:34:40 +0900

    Added instruction textView below the battery image.

--- app/src/main/java/com/codelab/batteryindicator/MainActivity.java
+++ app/src/main/java/com/codelab/batteryindicator/MainActivity.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final String CURRENT_LEVEL = "CURRENT_LEVEL";
@@ -15,7 +18,9 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton plusButton;
     private ImageButton minusButton;
     private ImageView batteryImage;
+    private TextView chargeLevelText;
     private int currentLevel;
+    List<String> levelList;
 
 
     @Override
@@ -26,12 +31,16 @@ public class MainActivity extends AppCompatActivity {
         plusButton = findViewById(R.id.plus_button);
         minusButton = findViewById(R.id.minus_button);
         batteryImage = findViewById(R.id.battery_image);
+        chargeLevelText = findViewById(R.id.text_charge_level);
 
-        //todo: optimize logic
+        levelList = Arrays.asList(getResources().getStringArray(R.array.charge_level));
+
         currentLevel = 0;
+        chargeLevelText.setText(levelList.get(0));
 
         if (savedInstanceState != null) {
             currentLevel = savedInstanceState.getInt(CURRENT_LEVEL);
+            chargeLevelText.setText(levelList.get(currentLevel));
         }
 
     }
@@ -39,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
-        outState.putInt(CURRENT_LEVEL,currentLevel);
+        outState.putInt(CURRENT_LEVEL, currentLevel);
         super.onSaveInstanceState(outState);
     }
 
@@ -48,15 +57,16 @@ public class MainActivity extends AppCompatActivity {
         if (currentLevel > 0) {
             currentLevel--;
             batteryImage.setImageLevel(currentLevel);
+            chargeLevelText.setText(levelList.get(currentLevel));
         }
     }
 
 
-
-    public void increase(View view){
-        if (currentLevel < 7){
+    public void increase(View view) {
+        if (currentLevel < 7) {
             currentLevel++;
             batteryImage.setImageLevel(currentLevel);
+            chargeLevelText.setText(levelList.get(currentLevel));
         }
     }
 
